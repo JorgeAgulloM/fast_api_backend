b1dbf8e5580fda0267790f71592a33571b22e727
Jorge1 <agullojorge@gmail.com>
2023-10-05 15:36:40 +0200

    add new endpoint

--- fast_api/main.py
+++ fast_api/main.py
@@ -6,3 +6,7 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "¡Hola FastAPI!"}
+
+@app.get("/url")
+async def url():
+    return {"url_project": "https://github.com/jorgeagullom/fast_api_backend"}
