e6758ea1ff0b5ba37feb0aa77280afefe361663a
Jorge1 <agullojorge@gmail.com>
2023-10-05 16:53:29 +0200

    refactored

--- fast_api/users.py
+++ fast_api/users.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 
+app = FastAPI()
+
 # Start server: from uvicorn users:app --reload
 # Stop server:  ctrl+c
 
@@ -17,9 +19,6 @@ user_list = [
     User(id = 1, name = "Yorch", surname = "Soft", age = 1, url = "https://softyorch.com")
 ]
 
-
-app = FastAPI()
-
 @app.get("/usersjson")
 async def usersjson():
     return [{"name": "Jorge", "surname": "Agullo", "age": 40, "url": "https://github.com/JorgeAgulloM"},
@@ -29,13 +28,20 @@ async def usersjson():
 async def users():
     return user_list
     
+# Path /user/1 -> return user id == 1
 @app.get("/user/{id}")
 async def user(id: int):
+    return search_user(id)
+    
+# Query /userquery/?id=1 -> return user id == 1
+@app.get("/userquery/")
+async def user(id: int):
+    return search_user(id)
+    
+def search_user(id: int):
     user = filter(lambda user: user.id == id, user_list)
     try:
         return list(user)[0]
     except Exception:
         return {"error": "No se ha encontrado al usuario {}".format(id)}
 
-    
-    
