67111be82d5d8918bf72589a33c08d59ca0dac25
Jorge1 <agullojorge@gmail.com>
2023-10-05 16:20:51 +0200

    Add user examples

--- fast_api/users.py
+++ fast_api/users.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 # Start server: from uvicorn users:app --reload
 # Stop server:  ctrl+c
+
+# Entity user
+class User(BaseModel):
+    name: str
+    surname: str
+    age: int
+    url: str
+    
+user_list = [
+    User(name = "Jorge", surname = "Agulló", age = 40, url = "https://github.com/JorgeAgulloM"),
+    User(name = "Yorch", surname = "Soft", age = 1, url = "https://softyorch.com")
+]
+
 
 app = FastAPI()
 
@@ -9,3 +23,9 @@ app = FastAPI()
 async def usersjson():
     return [{"name": "Jorge", "surname": "Agullo", "age": 40, "url": "https://github.com/JorgeAgulloM"},
             {"name": "Yorch", "surname": "Soft", "age": 1, "url": "https://softyorch.com"}]
+    
+    
+@app.get("/users")
+async def users():
+    return user_list
+    
