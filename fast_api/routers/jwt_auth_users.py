515f6926341a543a33085fa151afb94e60cce5ba
Jorge1 <agullojorge@gmail.com>
2023-10-06 19:26:35 +0200

    added api auth to router

--- fast_api/routers/jwt_auth_users.py
+++ fast_api/routers/jwt_auth_users.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException, status
 from pydantic import BaseModel
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from jose import jwt, JWTError
@@ -9,7 +9,7 @@ ALGORITHM ="HS256"
 ACCES_TOKEN_DURATION = 1
 SECRET = "075bcee72a66899888d94248ea24ba44f821eb8a1452ce44c3679b3687bc796c"
 
-app = FastAPI()
+router = APIRouter()
 
 oauth2 = OAuth2PasswordBearer(tokenUrl="login")
 crypt = CryptContext(schemes="bcrypt")
@@ -84,7 +84,7 @@ async def current_user(user: User = Depends(auth_user)):
     return user
 
 
-@app.post("/login")
+@router.post("/login")
 async def login(form: OAuth2PasswordRequestForm = Depends()):
     user_db = users_db.get(form.username)
 
@@ -107,6 +107,6 @@ async def login(form: OAuth2PasswordRequestForm = Depends()):
     return {"access_token":access_token, "token_type": "bearer"}
 
 
-@app.get("/users/me")
+@router.get("/users/me")
 async def me(user: User = Depends(current_user)):
     return user
