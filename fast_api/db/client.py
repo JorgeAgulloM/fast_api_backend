112716b602c003d69d8ac3df3446f8179d595ddc
Jorge1 <agullojorge@gmail.com>
2023-10-09 18:32:43 +0200

    Enviroment var configured

--- fast_api/db/client.py
+++ fast_api/db/client.py
@@ -1,10 +1,13 @@
 from pymongo import MongoClient
+import os
 
 # Local dataBase
 #db_client = MongoClient().local #Por defecto se conecta a localhost
 
 # Remote dataBase
+name_DB = os.environ.get('DATABASE_NAME')
+user_DB = os.environ.get('DATABASE_USER')
+pass_DB = os.environ.get('DATABASE_PASS')
+url_base = 'mongodb+srv://{}:{}@{}.mongodb.net/?retryWrites=true&w=majority'.format(user_DB, pass_DB, name_DB)
 
-url = 'mongodb+srv://<username>:<password>@cluster0.dmidmld.mongodb.net/?retryWrites=true&w=majority'
-
-db_client = MongoClient(url).db
+db_client = MongoClient(url_base).db
